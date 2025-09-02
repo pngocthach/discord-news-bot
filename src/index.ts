5fa2942b5132b0813327b340e4fa3493a5536ce0
pngocthach <jellypnt1311@gmail.com>
2025-09-02 12:23:51 +0700

    Initialize Discord News Bot project with essential files including .gitignore, package.json, pnpm-lock.yaml, README, tsconfig, and main application logic in index.ts. Set up dependencies for Hono and Discord.js, and configure basic server and bot functionality.

---
+++ src/index.ts
@@ -0,0 +1,61 @@
+import { serve } from "@hono/node-server";
+import { Hono } from "hono";
+import { Client, Events, GatewayIntentBits, IntentsBitField } from "discord.js";
+import { config } from "dotenv";
+config();
+
+const app = new Hono();
+
+const CHANNEL_ID = "1412106908394848347";
+const GUILD_ID = "1357691498367422664";
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    IntentsBitField.Flags.MessageContent,
+    IntentsBitField.Flags.GuildMessages,
+    IntentsBitField.Flags.GuildMembers,
+  ],
+});
+
+const TEST_MARKDOWN = `
+# Test
+## Test
+### Test
+#### Test
+##### Test
+###### Test
+`;
+
+client.once(Events.ClientReady, async (c) => {
+  console.log(`Logged in as ${c.user.displayName}`);
+  const channel = client.channels.cache.get(CHANNEL_ID);
+  if (channel) {
+    await channel.send(TEST_MARKDOWN);
+  }
+});
+
+client.on(Events.MessageCreate, async (message) => {
+  if (message.content === "/ping") {
+    await message.reply("Pong!");
+  }
+  if (message.content === "/dit me may") {
+    await message.reply("dit me may luon");
+  }
+});
+
+client.login(process.env.DISCORD_TOKEN);
+
+app.get("/", (c) => {
+  return c.text("Hello Hono!");
+});
+
+serve(
+  {
+    fetch: app.fetch,
+    port: 3000,
+  },
+  (info) => {
+    console.log(`Server is running on http://localhost:${info.port}`);
+  }
+);
