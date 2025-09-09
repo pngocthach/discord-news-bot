e9bab5ea15d4937951a4db5fe8a8c3bcf4dad9eb
pngocthach <jellypnt1311@gmail.com>
2025-09-09 20:23:29 +0700

    feat(puppeteer): add Puppeteer configuration and update build process
    - Introduced a new Puppeteer configuration file to specify cache directory for browser binaries.
    - Updated the build script in package.json to install Puppeteer browsers during the start process, ensuring the necessary dependencies are available for scraping operations.

---
+++ .puppeteerrc.cjs
@@ -0,0 +1,11 @@
+/** biome-ignore-all lint/style/useNodejsImportProtocol: config file*/
+"use strict";
+const { join } = require("path");
+
+/**
+ * @type {import("puppeteer").Configuration}
+ */
+module.exports = {
+  // Changes the cache location for Puppeteer.
+  cacheDirectory: join(__dirname, ".cache", "puppeteer"),
+};
